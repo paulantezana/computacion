b50eecaee4eccf910d4bc1098d7756c4ccfd8311
paul antezana <contacto@paulantezana.com>
2018-06-15 20:35:57 -0500

    new updates

--- bin/base.js
+++ bin/base.js
@@ -39,3 +39,41 @@ window.addEventListener('scroll', () => {
         }
     }
 });
+
+
+
+const getRemainTime = deadline => {
+    let now = new Date(),
+        remainTime = (new Date(deadline) - now + 1000) / 1000,
+        second = ('0' + Math.floor(remainTime % 60)).slice(-2),
+        minute = ('0' + Math.floor(remainTime  / 60 % 60)).slice(-2),
+        hours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
+        days = Math.floor(remainTime / (3600 * 24));
+    return {
+        remainTime,
+        second,
+        minute,
+        hours,
+        days
+    }
+}
+
+const countDown = (deadline, elem,  finalMessage) => {
+    let el = document.getElementById(elem);
+    if (el){
+        let et = document.createElement("span");
+        et.setAttribute("class","news-timer");
+        el.appendChild(et);
+
+        const timerUpdate = setInterval(()=>{
+            let t = getRemainTime(deadline);
+            et.innerHTML = ` ${t.days}d:${t.hours}h:${t.minute}m:${t.second}s`;
+            if (t.remainTime <= 1){
+                clearInterval(timerUpdate);
+                et.innerHTML = finalMessage;
+            }
+        },1000)
+    }
+}
+
+countDown("Jun 28 2018 19:00:00 GMT-0500","news-message","Llego la hora de la verdad");
